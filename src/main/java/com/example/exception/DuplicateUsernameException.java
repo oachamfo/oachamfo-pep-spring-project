c6ba6efadde6e5fc61c7a27758bf488e4f89422a
Owusuduah Achamfour <owusuachamfour@gmail.com>
2024-03-23 12:49:41 +0000

    Comments added to the DuplicateUsernameException class

--- src/main/java/com/example/exception/DuplicateUsernameException.java
+++ src/main/java/com/example/exception/DuplicateUsernameException.java
@@ -1,7 +1,10 @@
 package com.example.exception;
-
+/**
+ * Custom exception class for handling cases where a duplicate username is encountered.
+ */
 public class DuplicateUsernameException extends RuntimeException {
     public DuplicateUsernameException(String message) {
+       // Calls the constructor of the superclass (RuntimeException) with the provided message.
         super(message);
     }
 }
